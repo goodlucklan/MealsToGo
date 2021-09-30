e03f5ae0dc8ec71ea5c8c08cfdc1c1a1cef3c5b2
Luis Enrique Roman Pardo <43737954+goodlucklan@users.noreply.github.com>
2021-09-30 14:56:39 -0500

    Provider

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infraestructure/theme";
+
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
